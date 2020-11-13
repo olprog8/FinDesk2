1b3f10e87d78b0b3b382a2670620e3df4a8fdcac
olprog8 <solar2010@live.ru>
2020-11-13 14:40:09 +0300

    Рефакторинг0

--- FinDesk2/Startup.cs
+++ FinDesk2/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -28,6 +24,8 @@ namespace FinDesk2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            //Конвейер
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
