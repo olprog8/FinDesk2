f4eaa976acd65d997096dd638bb22817057dbcee
olprog8 <solar2010@live.ru>
2020-12-24 17:58:31 +0300

    Создал Контекст БД FinDeskDB класс

---
+++ FinDesk.DAL/Context/FinDeskDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+using FinDesk.Domain.Entities;
+
+namespace FinDesk.DAL.Context
+{
+    public class FinDeskDB: DbContext
+    {
+        public DbSet<BaseIssue> BaseIssues { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<IssueType> IssueTypes { get; set; }
+
+        public DbSet<IssueStatus> IssueStatuses { get; set; }
+        public FinDeskDB(DbContextOptions<FinDeskDB> Options) : base(Options)
+        {}
+
+
+    }
+}
