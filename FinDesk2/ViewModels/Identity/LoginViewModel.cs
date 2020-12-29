917080771815dd446cdeed70d49a38e83cb143c4
olprog8 <solar2010@live.ru>
2020-12-29 17:36:35 +0300

    Модели-представления механизма регистрации и авторизации

---
+++ FinDesk2/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinDesk2.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [MaxLength(256)]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+
+        //ПШ L6 1.34 Адрес на который нужно вернуться после того, как пользователь прошел авторизацию
+        //ПШ L6 при визуализации создасться скрытый элемент формы средствами MVC
+        [HiddenInput(DisplayValue = false)]
+        public string ReturnUrl { get; set; }
+
+    }
+}
