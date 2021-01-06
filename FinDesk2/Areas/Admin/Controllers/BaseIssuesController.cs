b5abd59b367454265760f16ccf74e6bebadee88d
olprog8 <solar2010@live.ru>
2021-01-06 16:23:09 +0300

    Список BaseIssues в области администратора

--- FinDesk2/Areas/Admin/Controllers/BaseIssuesController.cs
+++ FinDesk2/Areas/Admin/Controllers/BaseIssuesController.cs
@@ -8,12 +8,16 @@ using Microsoft.AspNetCore.Authorization;
 
 using FinDesk.Domain.Identity;
 
+using FinDesk2.Infrastructure.Interfaces;
+
 
 namespace FinDesk2.Areas.Admin.Controllers
 {
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class BaseIssuesController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IBaseIssuesData _BaseIssuesData;
+        public BaseIssuesController(IBaseIssuesData BaseIssuesData) { _BaseIssuesData = BaseIssuesData; }
+        public IActionResult Index() => View(_BaseIssuesData.GetBaseIssues());
     }
 }
