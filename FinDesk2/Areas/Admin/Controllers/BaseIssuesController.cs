830049a84fd8a4e821055f26d57cbcd04cc2a41c
olprog8 <solar2010@live.ru>
2021-01-07 13:24:49 +0300

    Заготовки методов редактирования и удаления элементов baseIssues

--- FinDesk2/Areas/Admin/Controllers/BaseIssuesController.cs
+++ FinDesk2/Areas/Admin/Controllers/BaseIssuesController.cs
@@ -10,6 +10,7 @@ using FinDesk.Domain.Identity;
 
 using FinDesk2.Infrastructure.Interfaces;
 
+using FinDesk.Domain.Entities;
 
 namespace FinDesk2.Areas.Admin.Controllers
 {
@@ -19,5 +20,32 @@ namespace FinDesk2.Areas.Admin.Controllers
         private readonly IBaseIssuesData _BaseIssuesData;
         public BaseIssuesController(IBaseIssuesData BaseIssuesData) { _BaseIssuesData = BaseIssuesData; }
         public IActionResult Index() => View(_BaseIssuesData.GetBaseIssues());
+
+        public IActionResult Edit(int? id)
+        {
+            var baseIssue = id is null ? new BaseIssue() : _BaseIssuesData.GetIssueById((int)id);
+
+
+            if (baseIssue is null)
+                return NotFound();
+                
+                return View(baseIssue);
+        }
+
+        public IActionResult Delete(int id) {
+      
+            var baseIssue = _BaseIssuesData.GetIssueById((int)id);
+
+
+            if (baseIssue is null)
+                return NotFound();
+
+            return View(baseIssue);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken,ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
+
+    
 }
